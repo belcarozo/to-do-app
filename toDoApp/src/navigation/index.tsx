7b5d5332de03e23adc69180344a20a650da2d2f0
Belen Carozo <belen.carozo@xmartlabs.com>
2021-05-14 14:37:02 -0300

    Added NewTask screen from header '+'  button

--- toDoApp/src/navigation/index.tsx
+++ toDoApp/src/navigation/index.tsx
@@ -4,26 +4,31 @@ import { Home } from '../screens/Home'
 import { styles } from './styles'
 import { Color } from '../styles/Pallete'
 import { Pressable, Text } from 'react-native'
+import { NewTask } from '../screens/NewTask'
+import { Details } from '../screens/Details'
 
 const Stack = createStackNavigator()
 
 export const AppContainer = () => {
-  const header = {
+  const header = ({ navigation }) => ({
+    //como tipar esto?
     title: 'Todo',
-    headerStyle: styles.header, //hmm esto va aca?
+    headerStyle: styles.header,
     headerTintColor: Color.almostWhite,
     headerRight: () => (
-      <Pressable style={styles.button}>
+      <Pressable
+        style={styles.button}
+        onPress={() => navigation.navigate('NewTask')}>
         <Text style={styles.buttonText}>+</Text>
       </Pressable>
     ),
-  }
+  })
 
   return (
     <Stack.Navigator initialRouteName="Home">
       <Stack.Screen name="Home" component={Home} options={header} />
-      {/* <Stack.Screen name="NewTask" component={NewTask} />
-    <Stack.Screen name="Details" component={Details} /> */}
+      <Stack.Screen name="NewTask" component={NewTask} />
+      <Stack.Screen name="Details" component={Details} />
     </Stack.Navigator>
   )
 }
