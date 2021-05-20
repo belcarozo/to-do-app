1812a2433fc9eaa7265b4b8b04a972f830db29f2
Belen Carozo <belen.carozo@xmartlabs.com>
2021-05-19 22:31:57 -0300

    about to implement redux state

---
+++ toDoApp/src/store/todo/index.tsx
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { RootState } from '..'
+import { todoState } from './types'
+
+const initialState: todoState = {
+  value: []
+}
+export const todoSlice = createSlice({
+  name: 'todo',
+  initialState: initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.value.push(action.payload)
+    },
+    checkUncheck: (state, action) => {
+      state.value[action.payload].checked = !state.value[action.payload].checked 
+    },
+    clearAllDone: (state) => {
+      state.value.filter((item) => !item.checked)
+    }
+  }
+})
+
+export const { addTodo, checkUncheck, clearAllDone } = todoSlice.actions
+
+export const selectTodo = (state: RootState) => state.counter.value
+
+export default todoSlice.reducer
---
+++ toDoApp/src/store/todo/types.tsx
@@ -0,0 +1,5 @@
+import { listItemType } from '../../screens/Home/types'
+
+export interface todoState {
+  value: listItemType[]
+}
